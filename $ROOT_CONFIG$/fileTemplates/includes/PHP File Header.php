94b0de9a0021d57573ec4cbdfdfae222cbc97cbe
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2015-11-06 23:36:41 +1300

    PS-10.0 <moo@mohameds-macbook-pro.local Update baseRefactoring.xml, find.xml, laf.xml, ide.general.xml, filetypes.xml, project.default.xml, databaseSettings.xml, git.xml, other.xml, editor.xml	Create frost.icls, PHP Function Doc Comment.php, Twilight_by_Rafi.icls, Default _2_.xml, Symfony2.icls, Default.xml, Drupal.xml, SS.xml, Default _3_.xml, GitHub-Remixed.icls, Default _4_.xml, PHP File Header.php, PHP Class.php, Default _1_.xml, Default copy.xml

---
+++ $ROOT_CONFIG$/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,8 @@
+/*
+ * This file is part of the ${PROJECT_NAME} package.
+ *
+ * (c) Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
---
+++ $ROOT_CONFIG$/fileTemplates/internal/PHP Class.php
@@ -0,0 +1,17 @@
+<?php
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+
+/**
+ * ${NAME} 
+ *
+ * @author Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
+ */
+class ${NAME} {
+
+}
